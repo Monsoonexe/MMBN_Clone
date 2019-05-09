9fefde8e9993d4361f22ad9f9f6f1efd2b4dad4d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-09 16:05:55 -0700

    Update GatherTargetsBehavior.cs
    added new fields and members

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/GatherTargetsBehavior.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/GatherTargetsBehavior.cs
@@ -2,6 +2,11 @@
 
 public abstract class GatherTargetsBehavior : ScriptableObject
 {
-    public abstract NaviController_Battle[] GatherTargets();
+    public bool canTargetSelf = false;//can hurt self?
+    public bool pierce = false;//hit more targets after first?
+    [Range(0, 4)]
+    public int pushback = 0;// 0 - none, 1 - push back 1 tile, 2 - back 2 tiles, 
+
+    public abstract NaviController_Battle[] GatherTargets(NaviController_Battle user);
 
 }
