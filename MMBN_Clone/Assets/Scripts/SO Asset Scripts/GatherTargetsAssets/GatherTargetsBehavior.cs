387fd49e316c0581a2b85a47394d36afd20df90a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-09 15:55:53 -0700

    Update GatherTargetsBehavior.cs
    base class never instantiated

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/GatherTargetsBehavior.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/GatherTargetsBehavior.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(menuName = "GatherTargetsBehavior/", fileName = "GatherTargetsBehavior")]
 public abstract class GatherTargetsBehavior : ScriptableObject
 {
     public abstract NaviController_Battle[] GatherTargets();
