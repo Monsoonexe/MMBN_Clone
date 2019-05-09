4e0f73b310d227c74c8f6b719ce784f9e38a01fb
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-09 16:05:39 -0700

    new blueprint for targeting behavior

---
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/FirstRaycast.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstRaycast : GatherTargetsBehavior
+{
+    public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
+    {
+        NaviController_Battle[] targetList;
+
+
+        return targetList;
+
+    }
+}
