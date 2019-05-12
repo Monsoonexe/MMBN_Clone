b01de63af44df0ac4e4c9e83c7770aab89a41616
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-12 00:15:09 -0700

    Update BaseAttack.cs
    added Element property

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/Attacks/BaseAttack.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/Attacks/BaseAttack.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 
 public abstract class BaseAttack : ScriptableObject{
-    public string attackName;
+    [SerializeField]
+    protected string attackName;
+
     /// <summary>
     /// Time in seconds that one must wait between successive shots.
     /// </summary>
     [Header("---Stats---")]
+    public Element element;
     public float delay = 0.25f;
+
     [Range(0.1f, 100)]
     /// <summary>
     /// How many seconds it takes to fully charge.  != 0
     /// </summary>
     public float chargeTime = 3;//multiplier
+
     public int damage = 1;
+
     public int chargeDamage = 3;
 
     [SerializeField]
@@ -20,6 +26,7 @@ public abstract class BaseAttack : ScriptableObject{
 
     [SerializeField]
     protected StatusEffect statusEffect;
+
     [SerializeField]
     protected StatusEffect statusEffect_Charged;
 
