96f419997c3667c38c9ee81033448e6362128368
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-11 23:01:49 -0700

    damage determiner class
    exists

---
+++ MMBN_Clone/Assets/Scripts/ElementalDamageManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class ElementalDamageManager
+{
+    public static float DetermineDamageModifier(Element attackerElement, Element receiverElement)
+    {
+        var damageModifier = 1.0f;
+        //TODO make damage chart / matrix
+
+        Debug.Log("Elemental damage not yet defined.  Modifier is always 1.0");
+
+        return damageModifier;
+    }
+}
