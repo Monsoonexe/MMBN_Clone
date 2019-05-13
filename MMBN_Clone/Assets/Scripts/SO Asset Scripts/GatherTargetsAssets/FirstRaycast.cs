4354914a26fcccc498d57b7028c5931f1646d2f3
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-12 20:35:57 -0700

    Update FirstRaycast.cs
    fixed raycasting direction

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/FirstRaycast.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/FirstRaycast.cs
@@ -6,26 +6,20 @@ public class FirstRaycast : GatherTargetsBehavior
     public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
     {
         NaviController_Battle[] targetList = new NaviController_Battle[1];//this type of attack can only ever hit one target
-
-        //get forward direction
-        //get start position
-        //fire ray
-        //if hit something
-        //return
-
+        
         Transform userXform = user.transform;
 
-        RaycastHit2D raycastHitInfo = Physics2D.Raycast(user.transform.position, user.transform.forward * user.GetOrientation(), PanelArray.globalScale);
+        RaycastHit2D raycastHitInfo = Physics2D.Raycast(user.transform.position, user.transform.right * user.GetOrientation(), PanelArray.globalScale);
 
         if (raycastHitInfo)
         {
-            //Debug.Log("ZAP! I shot and hit: " + raycastHitInfo.collider.name);
+            Debug.Log("ZAP! I shot and hit: " + raycastHitInfo.collider.name);
             targetList[0] = raycastHitInfo.collider.gameObject.GetComponent<NaviController_Battle>() as NaviController_Battle; 
         }
 
         else
         {
-            //Debug.Log("ZAP! I shot but missed.");
+            Debug.Log("ZAP! I shot but missed.");
         }
         
         return targetList;
