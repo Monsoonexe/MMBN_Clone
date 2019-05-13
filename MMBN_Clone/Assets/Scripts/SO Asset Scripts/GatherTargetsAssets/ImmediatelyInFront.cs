170e4781325e73f72691d112ea555315c8dbb75a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-12 22:02:37 -0700

    new assets and scripts
    new sword attack and behavior!

---
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/ImmediatelyInFront.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Basic Raycast Target", menuName = "Targeting Behaviors / Immediately in Front")]
+public class ImmediatelyInFront : GatherTargetsBehavior
+{
+
+    public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
+    {
+        NaviController_Battle[] targetList = new NaviController_Battle[1];//this type of attack can only ever hit one target
+
+        //this attack looks at the panel matrix
+        if (!panelArray)
+        {
+            FindPanelArray();
+        }
+        
+        //where is the user?
+        user.GetCurrentPanelCoordinates(out int attackerX, out int attackerY);
+
+        //ask the panel array what object is right in front of this user's coordinates
+        var occupantOnTargetPanel = panelArray.GetOccupantAtCoordinates(attackerX + user.GetOrientation(), attackerY);
+
+        if (occupantOnTargetPanel)
+        {//if something exists on this spot...
+            targetList[0] = occupantOnTargetPanel.GetComponent<NaviController_Battle>() as NaviController_Battle;
+        }
+                    
+        return targetList;
+    }
+}
