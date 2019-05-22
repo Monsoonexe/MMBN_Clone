8ef6f5ac421f4c19b1c9cca1da7bb2c934bab671
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-21 22:19:19 -0700

    Update ImmediatelyInFront.cs
    this is handled by base class on initialize

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/ImmediatelyInFront.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/ImmediatelyInFront.cs
@@ -7,13 +7,7 @@ public class ImmediatelyInFront : GatherTargetsBehavior
     public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
     {
         NaviController_Battle[] targetList = new NaviController_Battle[1];//this type of attack can only ever hit one target
-
-        //this attack looks at the panel matrix
-        if (!panelArray)
-        {
-            FindPanelArray();
-        }
-        
+                
         //where is the user?
         user.GetCurrentPanelCoordinates(out int attackerX, out int attackerY);
 
