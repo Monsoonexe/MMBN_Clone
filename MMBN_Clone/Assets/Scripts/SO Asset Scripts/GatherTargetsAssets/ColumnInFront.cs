dcd064c1c31501bc0d99c98c91b720a2be5600c6
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-22 18:48:07 -0700

    Update ColumnInFront.cs
    implemented GatherTargetPanels
    
    extended duplicibility by adding spacesInFront field

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/ColumnInFront.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/ColumnInFront.cs
@@ -6,12 +6,37 @@
 [CreateAssetMenu(fileName = "ColumnInFront_Behavior", menuName = "Targeting Behaviors / ColumnInFront")]
 public class ColumnInFront : GatherTargetsBehavior
 {
-    public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
+    private static readonly int maxTargets = 3;
+    /// <summary>
+    /// How many columns in front. 1 is immediately in front
+    /// </summary>
+    private static readonly int spacesInFront = 1;
+
+    /// <summary>
+    /// Get the panels this can target.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public override Panel[] GatherTargetPanels(NaviController_Battle user)
     {
-        var targetList = new NaviController_Battle[3];//this type of attack
+        var targetList = new Panel[maxTargets];//this type of attack
 
         user.GetCurrentPanelCoordinates(out int x, out int y);
-        x += user.GetOrientation();//get one column in front of user
+        x += user.GetOrientation() * spacesInFront;//get one column in front of user
+
+        targetList[0] = panelArray.GetPanel(x, y + 1);//one above
+        targetList[1] = panelArray.GetPanel(x, y);//on same row
+        targetList[2] = panelArray.GetPanel(x, y - 1);//one below
+
+        return targetList;
+    }
+
+    public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
+    {
+        var targetList = new NaviController_Battle[maxTargets];//this type of attack
+
+        user.GetCurrentPanelCoordinates(out int x, out int y);
+        x += user.GetOrientation() * spacesInFront;//get one column in front of user
 
         targetList[0] = panelArray.GetOccupantAtCoordinates(x, y + 1);//one above
         targetList[1] = panelArray.GetOccupantAtCoordinates(x, y);//on same row
