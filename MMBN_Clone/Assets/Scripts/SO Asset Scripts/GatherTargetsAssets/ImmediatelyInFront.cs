344c693f6fd6535ddf3b880f3f1f484264e88c18
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-22 18:48:50 -0700

    implemented GatherTargetPanels()

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/ImmediatelyInFront.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/ImmediatelyInFront.cs
@@ -3,16 +3,30 @@
 [CreateAssetMenu(fileName = "ImmediatelyInFront_Behavior", menuName = "Targeting Behaviors / Immediately in Front")]
 public class ImmediatelyInFront : GatherTargetsBehavior
 {
+    [SerializeField]
+    protected int squaresAhead = 1;
+
+    public override Panel[] GatherTargetPanels(NaviController_Battle user)
+    {
+        var panelList = new Panel[1];//this type of attack only hits one target
+        //where is the user?
+        user.GetCurrentPanelCoordinates(out int attackerX, out int attackerY);
+
+        //ask the panel array what object is right in front of this user's coordinates
+        panelList[0] = panelArray.GetPanel(attackerX + user.GetOrientation() * squaresAhead, attackerY);
+
+        return panelList;
+    }
 
     public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
     {
-        NaviController_Battle[] targetList = new NaviController_Battle[1];//this type of attack can only ever hit one target
+        var targetList = new NaviController_Battle[1];//this type of attack can only ever hit one target
                 
         //where is the user?
         user.GetCurrentPanelCoordinates(out int attackerX, out int attackerY);
 
         //ask the panel array what object is right in front of this user's coordinates
-        var occupantOnTargetPanel = panelArray.GetOccupantAtCoordinates(attackerX + user.GetOrientation(), attackerY);
+        var occupantOnTargetPanel = panelArray.GetOccupantAtCoordinates(attackerX + user.GetOrientation() * squaresAhead, attackerY);
 
         if (occupantOnTargetPanel)
         {//if something exists on this spot...
