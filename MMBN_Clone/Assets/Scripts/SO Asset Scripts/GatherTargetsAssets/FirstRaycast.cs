18b16d2f47bbdf31e5d91ed854ab53938d122da3
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-22 18:48:23 -0700

    implemented GatherTargetPanels
    implemented GatherTargetPanels

--- MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/FirstRaycast.cs
+++ MMBN_Clone/Assets/Scripts/SO Asset Scripts/GatherTargetsAssets/FirstRaycast.cs
@@ -3,6 +3,26 @@
 [CreateAssetMenu(fileName = "FirstRaycast_Behavior", menuName = "Targeting Behaviors / RaycastTarget")]
 public class FirstRaycast : GatherTargetsBehavior
 {
+    /// <summary>
+    /// MAX TARGETS 1
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public override Panel[] GatherTargetPanels(NaviController_Battle user)
+    {
+        var targets = GatherTargets(user);
+        var targetPanels = new Panel[1];//this type of attack only ever hits one target
+
+        targetPanels[0] = targets[0].GetCurrentPanel();
+        
+        return targetPanels;
+    }
+
+    /// <summary>
+    /// MAX TARGETS 1
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
     public override NaviController_Battle[] GatherTargets(NaviController_Battle user)
     {
         var targetList = new NaviController_Battle[1];//this type of attack can only ever hit one target
