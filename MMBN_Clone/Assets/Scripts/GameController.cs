c646818a58a79f1039868deb1da74bfa6802af6e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-01-02 14:43:48 -0800

    new game controller to exit app on escape

---
+++ MMBN_Clone/Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    private void Awake()
+    {
+        InitSingleton(this);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitGame();
+        }
+    }
+
+    private void ExitGame()
+    {
+        Debug.Log("Exiting Application...");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public static void InitSingleton(GameController instance)
+    {
+        if (!Instance)
+        {
+            Instance = instance;
+
+            DontDestroyOnLoad(instance.gameObject);//IMMORTALITY!
+        }
+        else
+        {
+            Destroy(instance.gameObject);
+        }
+
+    }
+}
